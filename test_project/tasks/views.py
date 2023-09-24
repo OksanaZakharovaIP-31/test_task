be07191d832a5e887a388127136e7e99d67e167d
Oksana <oksana2000-02@mail.ru>
2023-09-24 20:01:10 +0300

    Create entry.html, index.html, registration.html, sample.html files for registration and entry. Also setting urls for tasks app, create urls.py file for path in tasks app

--- test_project/tasks/views.py
+++ test_project/tasks/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'tasks/index.html')
+
+def entry(request):
+    return render(request, 'tasks/entry.html')
+
+def registration(request):
+    return render(request, 'tasks/registration.html')
